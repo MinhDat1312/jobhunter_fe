d80a1ce30a02f1db8c73551ef466fdb9e0483f5a
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-06-08 08:27:32 +0700

    account reducer

---
+++ src/redux/slice/accountSlice.ts
@@ -0,0 +1,130 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { callFetchAccount } from '../../config/api';
+
+interface IState {
+    isAuthenticated: boolean;
+    isLoading: boolean;
+    isRefreshToken: boolean;
+    errorRefreshToken: string;
+    user: {
+        userId: string;
+        email: string;
+        fullName: string;
+        role: {
+            roleId?: string;
+            name?: string;
+            permissions?: {
+                permissionId: string;
+                name: string;
+                apiPath: string;
+                method: string;
+                module: string;
+            }[]
+        }
+    };
+    activeMenu: string;
+}
+
+const initialState: IState = {
+    isAuthenticated: false,
+    isLoading: true,
+    isRefreshToken: false,
+    errorRefreshToken: "",
+    user: {
+        userId: "",
+        email: "",
+        fullName: "",
+        role: {
+            roleId: "",
+            name: "",
+            permissions: [],
+        },
+    },
+    activeMenu: 'home'
+};
+
+export const accountSlide = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {
+        setActiveMenu: (state, action) => {
+            state.activeMenu = action.payload;
+        },
+
+        setUserLoginInfo: (state, action) => {
+            state.isAuthenticated = true;
+            state.isLoading = false;
+            state.user.userId = action?.payload?.id;
+            state.user.email = action.payload.email;
+            state.user.fullName = action.payload.name;
+            state.user.role = action?.payload?.role;
+
+            if (!action?.payload?.user?.role) state.user.role = {};
+            state.user.role.permissions = action?.payload?.role?.permissions ?? [];
+        },
+
+        setLogoutAction: (state) => {
+            localStorage.removeItem('access_token');
+            state.isAuthenticated = false;
+            state.user = {
+                userId: "",
+                email: "",
+                fullName: "",
+                role: {
+                    roleId: "",
+                    name: "",
+                    permissions: [],
+                },
+            }
+        },
+
+        setRefreshTokenAction: (state, action) => {
+            state.isRefreshToken = action.payload?.status ?? false;
+            state.errorRefreshToken = action.payload?.message ?? "";
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchAccount.pending, (state, action) => {
+            if (action.payload) {
+                state.isAuthenticated = false;
+                state.isLoading = true;
+            }
+        })
+
+        builder.addCase(fetchAccount.fulfilled, (state, action) => {
+            if (action.payload) {
+                state.isAuthenticated = true;
+                state.isLoading = false;
+                state.user.userId = action?.payload?.user?.id;
+                state.user.email = action.payload.user?.email;
+                state.user.fullName = action.payload.user?.name;
+                state.user.role = action?.payload?.user?.role;
+
+                if (!action?.payload?.user?.role) state.user.role = {};
+                state.user.role.permissions = action?.payload?.user?.role?.permissions ?? [];
+            }
+        })
+
+        builder.addCase(fetchAccount.rejected, (state, action) => {
+            if (action.payload) {
+                state.isAuthenticated = false;
+                state.isLoading = false;
+            }
+        })
+    },
+
+});
+
+export const fetchAccount = createAsyncThunk(
+    'account/fetchAccount',
+    async () => {
+        const response = await callFetchAccount();
+        return response.data;
+    }
+)
+
+export const {
+    setActiveMenu, setUserLoginInfo, setLogoutAction, setRefreshTokenAction
+} = accountSlide.actions;
+
+export default accountSlide.reducer;
